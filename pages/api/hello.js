b9abfac41747d4a41d07cfc2e4e84502bd5a69c5
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2023-02-07 15:22:56 +0100

    v0.1
    Create home page and API route.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+
+  const { steamId } = req.body;
+  const profileSummaryResponse = await fetch(`https://rust.scmm.app/api/profile/${steamId}/summary`);
+  const profileSummaryData = await profileSummaryResponse.json();
+
+  const rustInventoryResponse = await fetch(`https://rust.scmm.app/api/profile/${profileSummaryData.steamId}/inventory/items?currency=eur`)
+  const rustInventoryData = await rustInventoryResponse.json();
+
+  res.json(rustInventoryData)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,143 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from './index.module.css'
+
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ steamId }) {
+
+  const [currentSteamId, setCurrentSteamId] = useState(steamId)
+  const [rustInventory, setRustInventory] = useState();
+
+  const handleChange = e => setCurrentSteamId(e.target.value)
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch("/api/hello",
+      {
+        body: JSON.stringify({ steamId: currentSteamId }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+      }
+    );
+    const data = await response.json()
+    console.log(data)
+    setRustInventory(data)
+    console.log(rustInventory)
+  }
+
+  // const DUMMY_DATA = [{
+  //   "quantity": 1,
+  //   "stacks": [
+  //     {
+  //       "steamId": "4006543796305807811",
+  //       "quantity": 1,
+  //       "tradableAndMarketable": true
+  //     }
+  //   ],
+  //   "averageBuyPrice": 0,
+  //   "originalPrice": 89,
+  //   "buyNowFrom": "SteamCommunityMarket",
+  //   "buyNowPrice": 1790,
+  //   "buyNowUrl": "https://steamcommunity.com/market/listings/252490/Blackout%20Hoodie",
+  //   "subscriptions": 31016,
+  //   "supplyTotalEstimated": 33287,
+  //   "supply": 45,
+  //   "demand": 29,
+  //   "actions": [
+  //     {
+  //       "icon": "fa-tools",
+  //       "name": "View Workshop",
+  //       "url": "https://steamcommunity.com/sharedfiles/filedetails/?id=2080975449"
+  //     },
+  //     {
+  //       "icon": "fa-balance-scale-left",
+  //       "name": "View Market",
+  //       "url": "https://steamcommunity.com/market/listings/252490/Blackout%20Hoodie"
+  //     },
+  //     {
+  //       "icon": "fa-shopping-cart",
+  //       "name": "View Store",
+  //       "url": "/store/2020-05-08-1835"
+  //     }
+  //   ],
+  //   "priceMovement": 1701,
+  //   "id": 3809161299,
+  //   "appId": 252490,
+  //   "name": "Blackout Hoodie",
+  //   "itemType": "Hoodie",
+  //   "hasGlow": false,
+  //   "backgroundColour": "#42413e",
+  //   "foregroundColour": "#a7ec2e",
+  //   "iconAccentColour": "#666666",
+  //   "iconUrl": "https://steamcommunity-a.akamaihd.net/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835BZ4WLGfCk4nReh8DEiv5dQOa0_rLY3QP5UgG1iqQ",
+  //   "timeCreated": "2020-05-01T21:19:39+12:00",
+  //   "timeAccepted": "2020-05-08T18:34:07+00:00"
+  // },
+  // {
+  //   "quantity": 16,
+  //   "stacks": [
+  //     {
+  //       "steamId": "4577332744578051421",
+  //       "quantity": 14,
+  //       "tradableAndMarketable": true
+  //     },
+  //     {
+  //       "steamId": "5555742936964020929",
+  //       "quantity": 1,
+  //       "tradableAndMarketable": true
+  //     },
+  //     {
+  //       "steamId": "5555742936970052055",
+  //       "quantity": 1,
+  //       "tradableAndMarketable": true
+  //     }
+  //   ],
+  //   "averageBuyPrice": 0,
+  //   "originalPrice": 210,
+  //   "buyNowFrom": "SteamCommunityMarket",
+  //   "buyNowPrice": 1396,
+  //   "buyNowUrl": "https://steamcommunity.com/market/listings/252490/Forest%20Raiders%20Pants",
+  //   "subscriptions": 27928,
+  //   "supplyTotalEstimated": 29761,
+  //   "supply": 49,
+  //   "demand": 33,
+  //   "actions": [
+  //     {
+  //       "icon": "fa-tools",
+  //       "name": "View Workshop",
+  //       "url": "https://steamcommunity.com/sharedfiles/filedetails/?id=2563935722"
+  //     },
+  //     {
+  //       "icon": "fa-balance-scale-left",
+  //       "name": "View Market",
+  //       "url": "https://steamcommunity.com/market/listings/252490/Forest%20Raiders%20Pants"
+  //     },
+  //     {
+  //       "icon": "fa-shopping-cart",
+  //       "name": "View Store",
+  //       "url": "/store/2021-08-12-2033"
+  //     }
+  //   ],
+  //   "priceMovement": 1186,
+  //   "id": 4532464795,
+  //   "appId": 252490,
+  //   "name": "Forest Raiders Pants",
+  //   "itemType": "Pants",
+  //   "hasGlow": false,
+  //   "backgroundColour": "#42413e",
+  //   "foregroundColour": "#a7ec2e",
+  //   "iconAccentColour": "#797C4F",
+  //   "iconUrl": "https://steamcommunity-a.akamaihd.net/economy/image/6TMcQ7eX6E0EZl2byXi7vaVKyDk_zQLX05x6eLCFM9neAckxGDf7qU2e2gu64OnAeQ7835da5WLEfCk4nReh8DEiv5daOqA7qLIwQPy8bbn-G7A",
+  //   "timeCreated": "2021-08-02T09:55:37+12:00",
+  //   "timeAccepted": "2021-08-12T20:32:01.8085788+00:00"
+  // }]
+
   return (
     <>
       <Head>
@@ -14,110 +146,38 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main>
+        <form>
+          <h1>Check your Steam inventory</h1>
+          <label htmlFor='steamId'>SteamID64</label>
+          <input type="text" id="steamId" name="steamId" required value={currentSteamId || ""} onChange={handleChange} />
+          <input type="submit" name="search" value="Search" onClick={handleSubmit} />
+        </form>
+
+        <div className='container'>
+          <div className={styles.inventory}>
+            {rustInventory?.map(item => (
+              <a href={item.buyNowUrl}>
+                <div className={styles.item} style={{ backgroundImage: `linear-gradient(45deg, ${item.iconAccentColour}1A 5%, transparent 55%, transparent 75%, ${item.iconAccentColour}1A 100%)` }}>
+                  <img src={item.iconUrl} />
+                  <p className={styles.item__name} style={{ color: item.foregroundColour }} >{item.name}</p>
+                  <p className={styles.item__qty}>{item.quantity}</p>
+                  <p></p>
+                  <p className={styles.item__price}>{(item.buyNowPrice / 100).toFixed(2)}€</p>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const steamId = process.env.STEAM_ID;
+  return {
+    props: { steamId: steamId }
+  }
 }
